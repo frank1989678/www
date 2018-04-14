227c97dfdf57ef840821c2deaf843fa3b7bda8ae
302976031 <302976031@qq.com>
2018-04-14 15:10:17 +0800

    n

--- php/ip.php
+++ php/ip.php
@@ -1,7 +1,7 @@
 <?php 
 $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 if (strpos($url, "localhost")) {
-  $ip = gethostbyname('USER-20180131DV');
+  // $ip = gethostbyname('USER-20180131DV');
   // header("location:http://".$ip."/");
 } 
 
