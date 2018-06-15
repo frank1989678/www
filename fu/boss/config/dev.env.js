3635a34952f71117c3559f1f8304a0b4582d990f
zhouxj <frank1989678@126.com>
2018-06-15 18:48:58 +0800

    1、cdk兑换页面 2、世界杯活动页

--- fu/boss/config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  BASE_API: '"http://10.0.3.31:8080"',
-  BASE_API2: '"http://wsifq5.natappfree.cc/"'
-})
